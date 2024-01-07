025b52d156f8840f3491ba6a083054eb813b1d29
Aditya Sudan <adityasudan21@gmail.com>
2024-01-07 20:17:09 +0530

    States and OnChange Functionality Created But Written Firebase auth import code in Login.js Which may give Problems because of the old nature of Firestore authentication process I saw on Youtube 🤧

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css";
+import { auth } from 'firebase/compat/auth';
 function Login() {
-    const loginToApp = () => { };
-    const register = () => { };
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const loginToApp = (e) => {
+        e.preventDefault();
+    };
+    const register = () => {
+
+    };
     return (
         <div className='login'>
             <img src='https://i.ibb.co/ZVcTWqm/1ax6e5c9.png' alt=''></img>
             <form action="">
-                <input placeholder='Full name (required if registering)' type="text" />
+                <input value={name} onChange={e => setName(e.target.value)} placeholder='Full name (required if registering)' type="text" />
                 <input placeholder='Profile Pic URL(Optional)' type="text" />
-                <input placeholder='Email' type="email" />
-                <input placeholder='Password' type="password" />
+                <input value={email} onChange={e => setEmail(e.target.value)} placeholder='Email' type="email" />
+                <input value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' type="password" />
                 <button type='submit' onClick={loginToApp}>Sign In</button>
             </form>
             <p>Not a Member? {"  "}
