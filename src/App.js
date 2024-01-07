2ad8d8cf3476801c5522c8990728c0c8e342bfa7
Aditya Sudan <adityasudan21@gmail.com>
2024-01-07 19:26:44 +0530

    Login Page Logic Made 🍅

--- src/App.js
+++ src/App.js
@@ -4,23 +4,30 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from './Feed';
 import Widget from "./Widget";
+import { useSelector } from 'react-redux';
+import { selectUser } from "./features/userSlice";
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
       { /* Header   */}
       <Header />
-      {/* App Body */}
-      <div className='app_body'>
-        {/* Siderbar */}
-        <Sidebar />
-        {/* Feed */}
-        <Feed />
-        {/* Widgets */}
-        <Widget/>
-        
-      </div>
-    </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app_body'>
+          {/* Siderbar */}
+          <Sidebar />
+          {/* Feed */}
+          <Feed />
+          {/* Widgets */}
+          <Widget />
+        </div>
+      )
+      }
+    </div >
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Login.css";
+function Login() {
+  return (
+    <div className=''>Login</div>
+  )
+}
+
+export default Login
