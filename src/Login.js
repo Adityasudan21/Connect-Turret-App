81a2c353b0747d874cd726c1a3a56d8b9b86b8b6
Aditya Sudan <adityasudan21@gmail.com>
2024-01-07 19:58:39 +0530

    Login Page Made🎈

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./Login.css";
 function Login() {
-  return (
-    <div className=''>Login</div>
-  )
+    const loginToApp = () => { };
+    const register = () => { };
+    return (
+        <div className='login'>
+            <img src='https://i.ibb.co/ZVcTWqm/1ax6e5c9.png' alt=''></img>
+            <form action="">
+                <input placeholder='Full name (required if registering)' type="text" />
+                <input placeholder='Profile Pic URL(Optional)' type="text" />
+                <input placeholder='Email' type="email" />
+                <input placeholder='Password' type="password" />
+                <button type='submit' onClick={loginToApp}>Sign In</button>
+            </form>
+            <p>Not a Member? {"  "}
+                <span className='login_register' onClick={register}>Register Now</span>
+            </p>
+        </div>
+    )
 }
 
 export default Login
