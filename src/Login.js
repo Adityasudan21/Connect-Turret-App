cecc98aa486d9eec6394222906f4ff697be4d399
Aditya Sudan <adityasudan21@gmail.com>
2024-01-08 17:09:39 +0530

    Added the Create User and Registering Functionality😒 Deep Firebase and Redux coding and making the Dispatch function

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,43 @@
 import React, { useState } from 'react'
 import "./Login.css";
-import { auth } from 'firebase/compat/auth';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { login } from './features/userSlice';
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
     const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
     const loginToApp = (e) => {
         e.preventDefault();
     };
     const register = () => {
+        if (!name) {
+            return alert("Please enter a full name!");
+        }
 
+        auth.createUserWithEmailAndPassword(email, password).then((userAuth) => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                photourl: profilePic,
+            })
+                .then(() => {
+                    dispatch(login({
+                        email: userAuth.user.email,
+                        uid: userAuth.user.uid,
+                        displayName: name,
+                        photourl: profilePic,
+                    }))
+                })
+        })
     };
     return (
         <div className='login'>
             <img src='https://i.ibb.co/ZVcTWqm/1ax6e5c9.png' alt=''></img>
             <form action="">
                 <input value={name} onChange={e => setName(e.target.value)} placeholder='Full name (required if registering)' type="text" />
-                <input placeholder='Profile Pic URL(Optional)' type="text" />
+                <input value={profilePic} onChange={e => setProfilePic(e.target.value)} placeholder='Profile Pic URL(Optional)' type="text" />
                 <input value={email} onChange={e => setEmail(e.target.value)} placeholder='Email' type="email" />
                 <input value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' type="password" />
                 <button type='submit' onClick={loginToApp}>Sign In</button>
